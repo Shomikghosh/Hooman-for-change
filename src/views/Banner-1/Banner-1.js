7916ac13c2228edd34964d8d5948d57cf85d2dc0
Shomikghosh <59383763+Shomikghosh@users.noreply.github.com>
2021-04-02 16:01:54 +0530

    cover photo added

--- src/views/Banner-1/Banner-1.js
+++ src/views/Banner-1/Banner-1.js
@@ -3,12 +3,15 @@ import './Banner-1.css';
 function Navbar() {
   return (
     <div className="Banner__whole">
-      {/* <div className="Banner-1">
-
-    </div> */}
+      <div className="Banner-1">
+      <div className="rule__banner">
+        
+        </div>
+    </div>
       <p className="hope">
           HOPE FOR HUMANITY
       </p>
+      
     </div>
     
     
